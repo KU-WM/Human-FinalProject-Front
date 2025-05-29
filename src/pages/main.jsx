3eb25a0918c70ffb5a852b291c5a4ac76a156a34
KU-WM <ku990732@gmail.com>
2025-05-29 15:43:45 +0900

    전송 방식 변경

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,17 +1,45 @@
-import { React } from "react";
+import axios from "axios";
+import { React, useCallback, useState } from "react";
 
 const MainPage = () => {
+  const [message, setMessage] = useState("");
+
+  const setData = (e) => {
+    console.log("message", e.target.value);
+    setMessage(e.target.value);
+  };
+
+  const sendData = async () => {
+    console.log("Send Data!");
+
+    try {
+      axios
+        .post("https://human-finalproject-back.onrender.com/user/answer", {
+          message: message,
+        })
+        .then((res) => {
+          console.log("Response: ", res);
+          return res.data;
+        });
+    } catch (error) {
+      console.log("Error ", error);
+      throw error;
+    }
+  };
+
   return (
     <>
       <main>
         <h3>Test Page</h3>
-        <form
-          action="https://human-finalproject-back.onrender.com/user/answer"
-          method="POST"
-        >
-          <input type="text" name="message" placeholder="생성 이미지 묘사" />
-          <button type="submit">submit</button>
-        </form>
+        <input
+          type="text"
+          name="message"
+          placeholder="생성 이미지 묘사"
+          onChange={setData}
+        />
+        <button type="button" onClick={sendData}>
+          생성
+        </button>
       </main>
     </>
   );
