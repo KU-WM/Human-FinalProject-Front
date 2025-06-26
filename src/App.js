4259eb3a995fd8e5bec63760baf38dae08faa57b
KU-WM <ku990732@gmail.com>
2025-06-26 17:29:58 +0900

    feat: add user uuid cookie init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import RootRoutes from "./routes/RootRoutes";
 import Header from "./pages/header";
+import refreshApi from "./component/refreshApi";
+import axios from "axios";
 
 function App() {
+
+  useEffect(() => {
+    const res = async() => {
+      await refreshApi.get(
+        "/user/cookieInit"
+      )
+      .then((res) => {
+        console.log("Cookie res1: ", res);
+        return res
+      })
+    }
+
+    res();
+  })
+
   return (
     <StrictMode>
       <Header />
